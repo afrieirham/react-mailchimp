cdd5d4b6f0dceaa2330396a36759b5b5b0e9d89f
Afrie Irham <afrieirham.work@gmail.com>
2022-08-24 11:39:05 +0800

    setup form ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-import React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  ChakraProvider,
+  theme,
+  Flex,
+  Input,
+  Button,
+  Box,
+} from '@chakra-ui/react';
 
 function App() {
-  return <ChakraProvider theme={theme}>Hello World</ChakraProvider>;
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    setLoading(true);
+    setLoading(false);
+  };
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box>
+        <Flex
+          as="form"
+          mx="auto"
+          height="100vh"
+          maxWidth="350px"
+          onSubmit={onSubmit}
+          direction="column"
+          justifyContent="center"
+        >
+          <Input
+            value={email}
+            placeholder="Enter your email"
+            onChange={e => setEmail(e.target.value)}
+          />
+          <Button
+            mt="2"
+            bg="black"
+            type="submit"
+            color="white"
+            isLoading={loading}
+            _hover={{ bg: 'gray.800' }}
+            _active={{ bg: 'gray.600' }}
+          >
+            Subscribe
+          </Button>
+        </Flex>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
